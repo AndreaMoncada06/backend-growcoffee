6e91553e0de4636745ee82c73819aaec7a718de7
Andrea <andreamoncada2006@gmail.com>
2025-09-10 07:48:31 -0500

    Configuración inicial de spring boot con conexión a mysql

---
+++ src/main/java/com/growcoffee/growcoffee/GrowcoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.growcoffee.growcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowcoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowcoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/growcoffee/growcoffee/GrowcoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growcoffee.growcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowcoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
